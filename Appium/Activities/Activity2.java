62fe586b169e7d61a463276941a229ee555911e4
Chitrat-17 <chitra.t@ibm.com>
2025-01-30 20:21:16 +0530

    Add files via upload

--- Appium/Activities/Activity2.java
+++ Appium/Activities/Activity2.java
@@ -4,7 +4,11 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.time.Duration;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,9 +19,10 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class Activity2 {
 	AppiumDriver driver;
+	WebDriverWait wait;
 	
 	@BeforeClass
-	public void setup() throws MalformedURLException, URISyntaxException {
+	public void setup() throws MalformedURLException, URISyntaxException, InterruptedException {
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setPlatformName("android");
 		options.setAutomationName("UiAutomator2");
@@ -27,12 +32,23 @@ public class Activity2 {
 		
 		URL serverURL = new URI("http://localhost:4723").toURL();
 		driver = new AndroidDriver(serverURL, options);	
+		driver.get("https://training-support.net");
+		Thread.sleep(10);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 	
   @Test
-  public void headingTest() {
-	  driver.get("https://training-support.net");
+  public void headingTest() throws InterruptedException {
+	  String pageHeading = driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"Training Support\"]")).getText();
+      System.out.println("Heading: " + pageHeading);
+	  driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"About Us\"]")).click();
+	  Thread.sleep(20);
 	  String pageTitle = driver.findElement(AppiumBy.xpath("//android.view.View[@text=\"About Us\"]")).getText();
 	  System.out.println("Heading : "+pageTitle);
   }
+  
+  @AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 }
