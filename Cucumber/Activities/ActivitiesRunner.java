68076215961b3c8b2ba305004679c2ccb159e0ca
Chitrat-17 <chitra.t@ibm.com>
2025-02-21 09:57:44 +0530

    Add files via upload

--- Cucumber/Activities/ActivitiesRunner.java
+++ Cucumber/Activities/ActivitiesRunner.java
@@ -18,17 +18,17 @@ import io.cucumber.junit.platform.engine.Constants;
 //  key = Constants.FILTER_TAGS_PROPERTY_NAME,
 //  value = "@activity4")
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("Features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions")
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity3")
-@ConfigurationParameter(
-	key = Constants.PLUGIN_PROPERTY_NAME,
-	value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json"
-)
-
-
-public class ActivitiesRunner {
-
-}
+//@Suite
+//@IncludeEngines("cucumber")
+//@SelectClasspathResource("features")
+//@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions")
+//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity3")
+//@ConfigurationParameter(
+//	key = Constants.PLUGIN_PROPERTY_NAME,
+//	value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json"
+//)
+//
+//
+//public class ActivitiesRunner {
+//
+//}
